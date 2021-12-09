88d8f58e389cba3edc9b5de559b6c38cdce04355
Hannah Wenger <hamecow@gmail.com>
2021-12-09 00:17:39 -0500

    Add socket.io connection to server.js and package.json with error

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const express = require("express"); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 8800; //Line 3
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(_dirname + "/index.html");
+});
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+const port = process.env.PORT || 50000; //Line 3
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
